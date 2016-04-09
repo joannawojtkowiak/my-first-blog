a45c81c86eba556d317a663df9a89ed6dda7aa3a
Joanna_Wojtkowiak <joanna.wojtkowiak@gmail.com>
2016-04-09 14:13:26 +0200

    My Django Girls app, first commit

---
+++ django_girls.py
@@ -0,0 +1,10 @@
+def hej(imie):
+    print('Hej ' + imie + '!')
+
+dziewczyny = ['Rachel', 'Monica', 'Phoebe', 'Ola', 'Ty']
+for imie in dziewczyny:
+    hej(imie)
+    print('Ejjjj!')
+
+for i in range(1, 6):
+    print(i)
